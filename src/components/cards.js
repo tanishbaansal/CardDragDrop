d614004cba94f62cb64a3327133c0e3d3ec602ee
Tanish <tanishbaansal@gmail.com>
2022-08-12 21:26:27 +0530

    Initial commit, contains basic box with the data , image and also sizing for rows

---
+++ src/components/cards.js
@@ -0,0 +1,68 @@
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Unstable_Grid2";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+}));
+
+const Cards = (props) => {
+    // First to distribute the content over different rows we
+    // calculate the total number of rows we need and split the data
+    // accordingly
+    const rows = [...Array(Math.ceil(props.data.length / 3))];
+    // chunk the data into the array of rows
+    const rowSplittedData = rows.map((row, idx) =>
+        props.data.slice(idx * 3, idx * 3 + 3)
+    );
+
+    // Created a variable to loop over the splitted chunk of data
+    // and adding the data item to the grid
+    // So this will allow us to split the data into groups of 3 and
+    // distributing the size of box based on that like for 2, 50-50%
+    // width will be automatically given cause of flex properties
+
+    let content = rowSplittedData.map((row, idx) => (
+        <Grid container spacing={2}>
+            {row.map((element) => (
+                <Grid key={idx} xs>
+                    <Item sx={{ p: 0 }}>
+                        <CardMedia
+                            component="img"
+                            image="https://mui.com/static/images/cards/contemplative-reptile.jpg"
+                            alt={element.title}
+                        />
+                        <CardContent>
+                            <Typography
+                                gutterBottom
+                                variant="h5"
+                                component="div"
+                            >
+                                {element.title}
+                            </Typography>
+                        </CardContent>
+                    </Item>
+                </Grid>
+            ))}
+        </Grid>
+    ));
+    console.log(content);
+    return (
+        // Creating a Container or box For the Data
+        <Box>
+            {/* Adding A grid to the box to add the cards(content) to it*/}
+            {/* Looping over the data we got and creating boxes from that data */}
+            {content}
+        </Box>
+    );
+};
+
+export default Cards;
